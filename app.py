eb3034da083e43fba14fb7697e5047c337b671b1
tanuj437 <tanuj.saxena.rks@gmail.com>
2024-07-24 23:19:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -261,7 +261,7 @@ for ex_human in human_list_path:
 image_blocks = gr.Blocks(theme="Nymbo/Alyx_Theme").queue()
 with image_blocks as demo:
     gr.HTML("<center><h1>Virtual Try-On 👕👔👚</h1></center>")
-    gr.HTML("<center><h1>Upload an image of a person and an image of a garment ✨</h1></center>")
+    gr.HTML("<center><p>Upload an image of a person and an image of a garment ✨</p></center>")
     with gr.Row():
         with gr.Column():
             imgs = gr.ImageEditor(sources='upload', type="pil", label='Human. Mask with pen or use auto-masking', interactive=True)
